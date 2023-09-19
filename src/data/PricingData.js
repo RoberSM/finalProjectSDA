7cabf8597d1a1a03ae9ed17c9a64e77ecff0d600
RobertMStan <stan.robertm@gmail.com>
2023-09-19 18:15:44 +0300

    first commit

---
+++ src/data/PricingData.js
@@ -0,0 +1,40 @@
+export const pricingData = [
+	{
+		title: 'Basic',
+		price: '17.25RON  user/month after offer period',
+		numAcc: '50-100 accounts',
+		features: ['Secure accounts', 'Best for small business', 'Easy', 'Affordable', '1GB RAM'],
+	},
+	{
+		title: 'Standard',
+		price: '20.55 ROn user/month after offer period',
+		numAcc: '50-500 accounts',
+		features: [
+			'Secure accounts',
+			'Best for small business',
+			'Customizable',
+			'3GB RAM',
+			'1TB SSD',
+		],
+	},
+	{
+		title: 'Premium',
+		price: '80.99 RON user/month after offer period',
+		numAcc: '50-1000 accounts',
+		features: [
+			'Secure accounts',
+			'Best for small business',
+			'Customizable',
+			'Easy Integration',
+			'Development Team',
+			'DNS & Domain included',
+		],
+	},
+
+	{
+		title: 'Enterprise',
+		price: '300RON user/month after offer period ',
+		numAcc: '1000+ Accounts',
+		features: ['Unlimited Storage', 'Scalability', 'Free DNS & Domain'],
+	},
+];
